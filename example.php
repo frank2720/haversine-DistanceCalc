e3b060ebfaa2ebc80a89996b1b83fe9789a77560
frank2720 <otienof534@gmail.com>
2024-06-23 12:46:26 +0300

    calculate distance in common units

--- example.php
+++ example.php
@@ -1,12 +1,46 @@
 <?php
+require __DIR__ ."/haversine.php";
 $latitude1 = 40.770623;
 $latitude2 = 40.758224;
 $longitude1 = -73.964367;
 $longitude2 = -73.917404;
 $distance = haversineDistance($latitude1, $latitude2, $longitude1, $longitude2);
-echo "$distance";
+
+echo "<pre>";
+print_r($distance);
+echo "</pre>";
+
+
+/**
+ * Output:
+ * 
+ * Array
+ *(
+ *  [distanceKms] => 4.188590914498
+ *  [distanceMiles] => 2.6026697209648
+ *  [distanceMeters] => 4188.590914498
+ *  [distanceYards] => 4580.6987253915
+ *)
+ */
+
+$distance_in_miles = $distance['distanceMiles']; 
+
+echo "<pre>";
+echo "$distance_in_miles miles";
+echo "</pre>";
 
 /**
  * Output: 
- * 4.188590914498
+ * 2.6026697209648 miles
+ */
+
+$distance_in_kilometers = $distance['distanceKms'];
+
+echo "<pre>";
+echo "$distance_in_kilometers kilometers";
+echo "</pre>";
+
+/**
+ * Output: 
+ * 4.188590914498 kilometers
  */
--- haversine.php
+++ haversine.php
@@ -1,6 +1,9 @@
 <?php
 
 const EARTH_RADIUS = 6371;
+const KM_MILES = 0.62137119;
+const KM_METERS = 1000;
+const KM_YARDS = 1093.6132983377;
 
 /**
  * Calculate the distance between two locations, given the latitude and longitude of both locations, and 
@@ -11,13 +14,27 @@ const EARTH_RADIUS = 6371;
  * @param float $lng1  longitude value of location 1 in degrees
  * @param float $lng2  longitude value of location 2 in degrees
  * 
- * @return float distance in Kilometers
+ * @return array distance in Kilometers, miles, meters, yards
  */
 
-function haversineDistance(float $lat1, float $lat2, float $lng1, float $lng2) : float
+function haversineDistance(float $lat1, float $lat2, float $lng1, float $lng2) : array
 {
     $distance = 2* EARTH_RADIUS *asin(sqrt((1-cos(deg2rad($lat2-$lat1))+cos(deg2rad($lat1))*cos(deg2rad($lat2))*(1-cos(deg2rad($lng2-$lng1))))/2));
-    return $distance;
+
+    $distance_kms = $distance;
+    $distance_miles = $distance_kms* KM_MILES;
+    $distance_meters = $distance_kms*KM_METERS;
+    $distance_yards = $distance_kms*KM_YARDS;
+
+    //$distance_units = compact('distance_kms','distance_miles','distance_meters','distance_yards');
+
+    $distance_units = [
+        'distanceKms'=> $distance_kms,
+        'distanceMiles'=> $distance_miles,
+        'distanceMeters'=> $distance_meters,
+        'distanceYards'=> $distance_yards
+    ];
+    return $distance_units;
 }
 
 
